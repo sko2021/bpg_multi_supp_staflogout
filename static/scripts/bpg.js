f41e419eb20bcdffba8c2afdf3107e24d08dee70
sko2021 <86480534+sko2021@users.noreply.github.com>
2023-01-22 08:30:53 -0500

    Update bpg.js

--- static/scripts/bpg.js
+++ static/scripts/bpg.js
@@ -26,3 +26,28 @@ catch(error){
 	console.log("Redirecting to AAD Logout")
     location.href = 'logout';
 }
+
+function logoutapp() {
+    console.log("Initiating Logout");
+    try {
+        for (i = 0; i < logoutUrls.length; i++) {
+        console.log("Calling URL[" + i + "]: " + logoutUrls[i]);
+        $.ajax({
+            url: logoutUrls[i],
+            type: "GET",
+			async: false,
+        	cache: false,
+        	timeout: 30000,
+            success: function(data, status, jqXHR) {
+                console.log("Status[" + i + "]: " +  jqXHR.status);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+    }
+}
+catch(error){
+    console.log(error);
+}
+}
